29998a3f277ec35ae154a38813dd9325a07ddf4c
CHIRAG <freeautomationlearning@gmail.com>
2017-06-21 23:47:32 +0530

    Tutorial 2 code is pushed

---
+++ src/com/freeautomationlearning/Tutorial2.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.freeautomationlearning;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author CHIRAG
+ *
+ */
+public class Tutorial2 {
+
+	@Test
+	public void firstTestNGRun()
+	{
+		System.out.println("Testing is Good !!");
+	}
+}
