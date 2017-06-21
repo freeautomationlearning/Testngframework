f3f1abdf307047dcaff13f08278f825fb5559dfc
CHIRAG <freeautomationlearning@gmail.com>
2017-06-21 23:10:49 +0530

    Test Commit

---
+++ src/com/freeautomationlearning/TestClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.freeautomationlearning;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author CHIRAG
+ *
+ */
+public class TestClass {
+
+	@Test
+	public void testNG()
+	{
+		System.out.println("Test NG");
+	}
+}
